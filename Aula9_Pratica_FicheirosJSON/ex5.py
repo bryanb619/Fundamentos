6d06ae2a8e01b6bded6b4d868fcb10c76242b088
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2023-12-03 15:23:17 +0000

    Ex 5 aula 9: Resolvido e comentado´

--- Aula9_Pratica_FicheirosJSON/ex5.py
+++ Aula9_Pratica_FicheirosJSON/ex5.py
@@ -1,11 +1,27 @@
 import json
 
-file_name = "text_data"
+""" 1. Path to the file
+    2. open file i read mode
+    3. read the file we use {json_ data} 
+        that will be our data to be read from the file
+    4. data variable will be a dictionary that receives the info
+        json.loads(_json_data_) pushes info to the data variable (our new dictionary)
+    5. print data
 
-file = open(file_name +".json", "rt")
+"""
+# 1.
+json_path = "D:\Projects\Exercises\FP\Aula9_Pratica_FicheirosJSON/text_data.json"
+ 
+# 2.
+file = open(json_path, "r")
 
-data = json.load(file)
+# 3.
+json_data = file.read()
 
-print(f"This is JSON file{data}")
+# 4.
+data = json.loads(json_data)
 
-file.close()
+# 5.
+print(data)
+
+
