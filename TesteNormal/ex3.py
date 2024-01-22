deed4618bf7b84ccb6508d6d99cc4deca5f802ef
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-01-22 18:01:41 +0000

    Ex 3 solução alternativa

--- TesteNormal/ex3.py
+++ TesteNormal/ex3.py
@@ -8,7 +8,13 @@ ou a linha não existir.
 
 """ soma todos os elementos de uma coluna de uma matriz:
 
-    1. Se a coluna não existir, retornar 0 (inteiro zero)
+    1. Se a coluna não existir, retornar 0 (inteiro zero)  
+    
+    OBS :> if you want to check if col is out of bounds 
+    (i.e., if it's not a valid column index), 
+    you should use if col >= len(m[0]). 
+    This condition will return True if col is greater than or equal to the number of columns, 
+    which means that col is not a valid column index.
     
     2. Iterar sobre a matriz e declarar uma variável para guardar a soma dos elementos da coluna:
         Este loop é feito com o tamanho da matriz
@@ -21,7 +27,7 @@ ou a linha não existir.
 def sum_column(m, col):
     
     # 1.
-    if col > len(m[0]):
+    if col >= len(m[0]):
         return 0
     
     # 2.
@@ -58,6 +64,7 @@ def sum_row(m, row):
     # 2.
     for i in range(len(m)):
         
+        num = 0 
         # 2.a
         if i == row:
             # 2.b
@@ -84,5 +91,86 @@ print (sum_row(grid , 2))
 #print (sum_row(grid , 8))
 
 " sum_column "
-print("Soma de coluna: ")
+print("Soma de coluna:  'Versão 1'")
 print(sum_column(grid , 2)) # deve retornar 23
+#---------------------------------------------------------#
+
+
+" Versão alternativa "
+
+
+""" Esta Versão certifica que tudo esta correto dentro da matriz (Certifica que existe o numero colunas apropriado em cada linha)
+
+    1. variável local para incrementação
+    
+    2. Iteração com alcance máximo em tamanho da matriz
+    3 e 4. 
+    - Se a coluna não existir, continuar e incrementar sum_total com 0, (0+0 = 0)
+    - Se a coluna existir incrementar com valor solicitado em matriz[posição de linha] e [posição de coluna]
+    
+    5. retornar a soma seja qual for o caso... 
+
+"""
+def sum_column2(m, col):
+
+    # 1.
+    sum_total = 0
+    
+    # 2. 
+    for i in range(len(m)):
+        
+        # 3.
+        if col >= len(m[i]):
+            continue
+        # 4.
+        sum_total += m[i][col]
+
+    # 5. 
+    return sum_total
+        
+     
+
+""" 
+    1.Verficar se a linha existe
+    
+    2. criar uma variável para guardar a soma dos elementos da linha
+    
+    3. Iterar sobre a lista na posição param: row (ou seja, apenas buscar elementos na linha pedida)
+    4. Somar entao a cada loop o numero obtido na linha(row)
+    5. Retornar o total 
+    
+param: m: matrix
+param: row: row 
+"""
+def sum_row2(m, row):
+    
+    # 1.
+    if row > len(m):
+        return 0
+    
+    # 2. 
+    total_sum = 0
+
+    # 3. 
+    for number in m[row]:
+        # 4.
+        total_sum += number
+            
+    return total_sum
+
+
+
+
+" sum_column "
+print("Soma de coluna:  'Versão 2'")
+print(sum_column2(grid , 2)) # deve retornar 23
+    
+print("Soma de linha: 'Versão 2'")
+print(sum_row2(grid , 2))
+
+
+
+
+
+        
+    
