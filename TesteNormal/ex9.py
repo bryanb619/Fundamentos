c7076287e5f3fca8cf58d4d9db89cc31d7e66e34
Steven Hall <73695945+bryanb619@users.noreply.github.com>
2024-01-23 12:56:50 +0000

    ex 8 Alternativo em Falta + ex 9 extra

--- TesteNormal/ex9.py
+++ TesteNormal/ex9.py
@@ -15,9 +15,35 @@ def process(list, function):
     
     # 1. 
     return [function(x) for x in list]
-    
+
     
 
 print(process(["abc","def","ghi"], lambda x: x * 2))
 
 print(process(["abc","defdef","ghij"], lambda x: len(x)))
+
+
+
+"EXTRA"
+
+"""
+Como chamar lambda com mais do que um argumento
+e retornar o resultado da função lambda
+
+"""
+def process_multiple(a,y, func):
+    
+    return func(a,y)
+
+
+add = lambda a,b: a+b
+mul = lambda a,b: a*b
+
+print(process_multiple(5,2,add))
+print(process_multiple(3,3, mul))
+
+print("""
+--------------------------------
+ """)
+
+#print(process_multiple(5,2,lambda a,b: a+b))
